90bc79f4c1cc3d9e220e22821067d7e65dfaa5f1
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-09-20 17:57:36 +0500

    auth complete with frontend

---
+++ client/src/pages/index.js
@@ -0,0 +1,44 @@
+import AdminDashboard from "./admin/AdminDashboard";
+import ManageOrders from "./admin/ManageOrders";
+import ManageUsers from "./admin/ManageUsers";
+import TourDetailsAdmin from "./admin/TourDetailsAdmin";
+import Error from "../shared/Error";
+import Verify from "./auth/Verify";
+import ForgotPassword from "./auth/ForgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import OrderDetailsPage from "./profile/OrderDetailsPage.jsx";
+import UpdateProfile from "./profile/UpdateProfile";
+import TourVendorDetails from "./Profile/TourVendorDetails";
+import OrdersPage from "./profile/OrdersPage.jsx";
+import TouristDashboard from "./tourist/TouristDashboard";
+import CreateTour from "./vendor/CreateTour";
+import UpdateTour from "./vendor/UpdateTour";
+import VendorDashboard from "./vendor/VendorDashboard";
+import AttractionList from "./attraction/AttractionList";
+import PlaceDetails from "./attraction/PlaceDetails";
+import Spinner from "../components/Spinner/Spinner";
+export {
+  AdminDashboard,
+  ManageUsers,
+  ManageOrders,
+  TourDetailsAdmin,
+  OrderDetailsPage,
+  OrdersPage,
+  UpdateProfile,
+  TourVendorDetails,
+  TouristDashboard,
+  Error,
+  Verify,
+  ForgotPassword,
+  ResetPassword,
+  Login,
+  Register,
+  CreateTour,
+  UpdateTour,
+  VendorDashboard,
+  AttractionList,
+  PlaceDetails,
+  Spinner,
+};
