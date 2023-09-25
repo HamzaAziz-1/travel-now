af8384b2393b675156de441a4a713f29379d9b72
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-09-25 15:22:16 +0500

    Update Tours.jsx

--- client/src/pages/Tours.jsx
+++ client/src/pages/Tours.jsx
@@ -10,7 +10,7 @@ import { useGlobalContext } from "../context/AuthContext";
 
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1); // Start from page 1
   const useFetch = (url) => {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
@@ -23,12 +23,12 @@ const Tours = () => {
           const res = await fetch(url);
 
           if (!res.ok) {
-            setError("failed to fetch");
+            setError("Failed to fetch");
+          } else {
+            const result = await res.json();
+            setData(result.tours);
           }
-          const result = await res.json();
-          console.log(result.tours);
 
-          setData(result.tours);
           setLoading(false);
         } catch (err) {
           setError(err.message);
@@ -56,8 +56,13 @@ const Tours = () => {
   useEffect(() => {
     const pages = Math.ceil(tourCount / 8);
     setPageCount(pages);
-    window.scrollTo(0, 0);
-  }, [page, tourCount, tours]);
+  }, [tourCount]);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= pageCount) {
+      setPage(newPage);
+    }
+  };
 
   return (
     <>
@@ -78,13 +83,48 @@ const Tours = () => {
           )}
           {error && <h4 className="text-center pt-5">{error}</h4>}
           {!loading && !error && (
-            <Row>
-              {tours?.map((tour) => (
-                <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
-                  <TourCard tour={tour} tourVendor={tour.vendor} />
-                </Col>
-              ))}
-            </Row>
+            <>
+              <Row>
+                {tours?.map((tour) => (
+                  <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+                    <TourCard tour={tour} tourVendor={tour.vendor} />
+                  </Col>
+                ))}
+              </Row>
+              {!loading && tours.length === 0 && (
+                <p className="text-center">No tours found.</p>
+              )}
+              {/* Pagination */}
+              <div className="text-center mt-4">
+                <button
+                  className="btn btn-outline-primary mx-1"
+                  onClick={() => handlePageChange(page - 1)}
+                  disabled={page === 1}
+                >
+                  &lt; Prev
+                </button>
+                {Array.from({ length: pageCount }, (_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handlePageChange(index + 1)}
+                    className={`btn ${
+                      page === index + 1
+                        ? "btn-primary active__page"
+                        : "btn-outline-primary"
+                    } mx-1`}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+                <button
+                  className="btn btn-outline-primary mx-1"
+                  onClick={() => handlePageChange(page + 1)}
+                  disabled={page === pageCount}
+                >
+                  Next &gt;
+                </button>
+              </div>
+            </>
           )}
         </Container>
       </section>
