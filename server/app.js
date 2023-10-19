45cbd1792c5c786f9f9b096b5ed0f4f7b893f80a
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-10-19 16:29:46 +0500

    add socket

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
-require("dotenv").config();
 require("express-async-errors");
+const dotenv = require("dotenv").config();
 // express
 
 const express = require("express");
 const app = express();
+
 // rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
 const mongoSanitize = require("express-mongo-sanitize");
-
 // database
 const connectDB = require("./db/connect");
 
@@ -41,6 +42,7 @@ app.use(mongoSanitize());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(bodyParser.json());
 
 app.use(express.static("./public"));
 
@@ -60,7 +62,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-
+console.log(process.env.PORT);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
