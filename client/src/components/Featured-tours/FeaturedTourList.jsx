159c17e042a0c27fc6e5785421441b587c8b1454
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-10-21 16:11:52 +0500

    Update FeaturedTourList.jsx

--- client/src/components/Featured-tours/FeaturedTourList.jsx
+++ client/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from "react";
 import TourCard from "../../shared/TourCard";
 import { Col } from "react-bootstrap";
 import Spinner from "../Spinner/Spinner";
+import useFetch from '../../hooks/useFetch'
 
 const FeaturedTourList = () => {
-  const useFetch = (url) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const res = await fetch(url);
-
-          if (!res.ok) {
-            setError("failed to fetch");
-          }
-          const result = await res.json();
-          console.log(result.tours);
-
-          setData(result.tours);
-          setLoading(false);
-        } catch (err) {
-          setError(err.message);
-          setLoading(false);
-        }
-      };
-
-      fetchData();
-    }, [url]);
-
-    return {
-      data,
-      error,
-      loading,
-    };
-  };
-
+ 
   const {
     data: featuredTours,
     loading,
