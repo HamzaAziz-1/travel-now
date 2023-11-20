f19162fd6bf46510410dceae263ec3dd6103418c
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-11-20 18:22:14 +0500

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,32 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-     senderId : {
-          type : String,
-          required : true
-     },
-     senderName : {
-          type: String,
-          required : true
-     },
-     receiverId : {
-          type: String,
-          required : true          
-     },
-     message : {
-          text : {
-               type: String,
-               default : ''
-          },
-          image : {
-               type : String,
-               default : ''
-          }           
-     },
-     status :{
-          type : String,
-          default : 'unseen'
-     }
-},{timestamps : true});
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+    senderName: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    message: {
+      text: {
+        type: String,
+        default: "",
+      },
+      image: {
+        type: String,
+        default: "",
+      },
+    },
+    status: {
+      type: String,
+      default: "unseen",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Message', messageSchema);
+module.exports = mongoose.model("Message", messageSchema);
